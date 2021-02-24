da57ac51c4e106d7323531ebb61e26b6c46a895b
Kevin Heard <kevin.heard@travelctm.com>
2021-02-24 18:45:45 +1100

    svelte snowpack initial

---
+++ src/Child.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+  import ConfettiGenerator from 'confetti-js';
+
+  onMount(() => {
+    const confettiSettings = {
+      target: 'my-canvas',
+      width: 300,
+      height: 100
+    };
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+  });
+</script>
+
+<p>this is a child component. confetti:</p>
+
+<canvas id="my-canvas"></canvas>
+
